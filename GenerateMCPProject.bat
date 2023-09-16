d7bf91dc9bf675db164e7254225b04b387ecd603
nicholaszollinger <nicholas.zollinger@gmail.com>
2023-09-15 22:30:58 -0700

    Changing the Folder Structure, and Removing the Tools
    I have been trying to get my build system up and running correctly and as a result, the Tools folder is going to live outside of the Engine.
    
    I have made a script to generate necessary folders and template files for a new project using my engine (basically a primitive engine launcher). To get things to work nicely, I needed to remove the additional 'MCPEngine' folder. However, when my launcher downloads the engine, the engine is placed in a MCPEngine folder. Basically, I had a double situation. This is cleaner.

--- GenerateMCPProject.bat
+++
@@ -1,4 +0,0 @@
-:: Creates a sln file with the engine project already setup. You must pass in a project name.
-
-CALL Tools\Premake\premake5.exe --file="Tools\CreateNewProject.lua" vs2022 %1 %CD%
-PAUSE
